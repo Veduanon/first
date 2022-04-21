5e6ccfed12b9a9eeaa825be7a3cca923cb5d015b
VeduanonFirst VeduanonLast <dimashquwert@gmail.com>
2022-04-21 15:56:27 +0600

    ADMIN

--- admin/js/script2.js
+++ admin/js/script2.js
@@ -1,12 +1,23 @@
-var auth = firebase.auth();
+ // Import the functions you need from the SDKs you need
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ const firebaseConfig = {
+    apiKey: "AIzaSyDE6cFF7klfN0XeUsLT6wstMQzfnG5uxTY",
+    authDomain: "vnfdin-b8954.firebaseapp.com",
+    projectId: "vnfdin-b8954",
+    storageBucket: "vnfdin-b8954.appspot.com",
+    messagingSenderId: "1084761966152",
+    appId: "1:1084761966152:web:e6fddc1644ab9c841afa7d",
+    measurementId: "G-VWPRTXX3KG"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
 function sendEmail(){
-    email=document.getElementById('typeEmail').value;
+    const email=document.getElementById('typeEmail').value;
+    alert(email)
 }
-var emailAddress = "email";
-auth.sendPasswordResetEmail(emailAddress)
-.then(function() {  
-    alert('email sent')
-})
-.catch(function(error) {
-    alert("an error happend")
-});
