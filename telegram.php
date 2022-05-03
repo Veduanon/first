f248317125946b0b66618bbfdf14c56564b102aa
ShisuiZH ShisuiZH <fugatyi@gmail.com>
2022-05-03 23:10:26 +0600

    aga

---
+++ telegram.php
@@ -0,0 +1,30 @@
+<?php
+// токен после bot
+/* https://api.telegram.org/bot5222133279:AAFpjNhsMuDOEVEqR3k39PskNhL66oU6plw/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['user_name'];
+$email = $_POST['user_email'];
+$theme = $_POST['user_theme'];
+$message = $_POST['user_message'];
+$token = "5222133279:AAFpjNhsMuDOEVEqR3k39PskNhL66oU6plw";
+$chat_id = "-709660835";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Email: ' => $email,
+  'Тема: ' => $theme,
+  'Сообщение: ' => $message
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: index.html');
+} else {
+  echo "Error";
+}
+?>
