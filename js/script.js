10b850d7957420ad80ac4511f3394db0c6a0fded
ShisuiZH ShisuiZH <fugatyi@gmail.com>
2022-05-16 15:39:45 +0600

    jdfdaj

--- js/script.js
+++ js/script.js
@@ -37,11 +37,34 @@ $(document).ready(function(){
                 slidesToScroll: 1,
               }
             }
-            // You can unslick at a given breakpoint now by adding:
-            // settings: "unslick"
-            // instead of a settings object
           ],
     });
+});
+
+$(document).ready(function(){
+  $('.slider_2').slick({
+      infinite: true,
+      speed: 450,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      dots: true,
+      arrows: false,
+      responsive: [
+          {
+            breakpoint: 1224,
+            settings: {
+              
+            }
+          },
+          {
+            breakpoint: 850,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+            }
+          },
+        ],
+  });
 });
 
 
@@ -74,5 +97,24 @@ $(window).on('load', function() {
   $preloader = $('.preloader'),
   $loader = $preloader.find('.preloader');
   $loader.fadeOut();
-  $preloader.delay(2000).fadeOut('slow');
+  $preloader.delay(1500).fadeOut('slow');
 });
+
+// document.body.onload = function()
+// {
+//     $(".dot-1").css("animation", "dot-light 1s 0s forwards");
+//     $(".dot-2").css("animation", "dot-light 1.5s 0.75s forwards");
+//     $(".dot-3").css("animation", "dot-light 1.5s 1.5s forwards");
+//     $(".dot-4").css("animation", "dot-light 1.5s 2.25s forwards");
+//     setTimeout(function(){$(".item-1").css("width", "0");}, 4000);
+//     setTimeout(function(){$(".item-2").css("height", "0");}, 5000);
+//     setTimeout(function(){$(".item-3").css("height", "0");}, 5300);
+//     setTimeout(function(){$(".item-4").css("height", "0");}, 5500);
+ 
+//     setTimeout(function(){$(".header").css({"opacity":"1", "transform":"translate(0)"});}, 6100);
+//     setTimeout(function(){$(".p-1").css({"opacity":"1", "transform":"translate(0)"});}, 6300);
+//     setTimeout(function(){$(".p-2").css({"opacity":"1", "transform":"translate(0)"});}, 6500);
+//     setTimeout(function(){$(".p-3").css({"opacity":"1", "transform":"translate(0)"});}, 6700);
+ 
+//     setTimeout(function(){$(".preloader").css("visibility", "hidden");}, 7000);
+// }
