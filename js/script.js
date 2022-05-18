3f81ef2886304d92409cc55f81ce961868070201
ShisuiZH ShisuiZH <fugatyi@gmail.com>
2022-05-18 15:45:10 +0600

    ggg

--- js/script.js
+++ js/script.js
@@ -84,6 +84,7 @@ $(function(){
 $(function(){
   $('.button_2_1').click(function(){
       $('.nav_ul_2').toggleClass('none_2');
+      $('.phone-button').toggleClass('none');
   });
 });
 
@@ -99,22 +100,3 @@ $(window).on('load', function() {
   $loader.fadeOut();
   $preloader.delay(1500).fadeOut('slow');
 });
-
-// document.body.onload = function()
-// {
-//     $(".dot-1").css("animation", "dot-light 1s 0s forwards");
-//     $(".dot-2").css("animation", "dot-light 1.5s 0.75s forwards");
-//     $(".dot-3").css("animation", "dot-light 1.5s 1.5s forwards");
-//     $(".dot-4").css("animation", "dot-light 1.5s 2.25s forwards");
-//     setTimeout(function(){$(".item-1").css("width", "0");}, 4000);
-//     setTimeout(function(){$(".item-2").css("height", "0");}, 5000);
-//     setTimeout(function(){$(".item-3").css("height", "0");}, 5300);
-//     setTimeout(function(){$(".item-4").css("height", "0");}, 5500);
- 
-//     setTimeout(function(){$(".header").css({"opacity":"1", "transform":"translate(0)"});}, 6100);
-//     setTimeout(function(){$(".p-1").css({"opacity":"1", "transform":"translate(0)"});}, 6300);
-//     setTimeout(function(){$(".p-2").css({"opacity":"1", "transform":"translate(0)"});}, 6500);
-//     setTimeout(function(){$(".p-3").css({"opacity":"1", "transform":"translate(0)"});}, 6700);
- 
-//     setTimeout(function(){$(".preloader").css("visibility", "hidden");}, 7000);
-// }
