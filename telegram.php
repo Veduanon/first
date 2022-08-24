6bdcd16338e863ca190a42bb5941cc19bf34db61
Veduanon <dimashquwert@gmail.com>
2022-08-25 00:24:15 +0600

    as

--- telegram.php
+++ telegram.php
@@ -3,18 +3,16 @@
 /* https://api.telegram.org/bot5222133279:AAFpjNhsMuDOEVEqR3k39PskNhL66oU6plw/getUpdates,
 где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
 
-$name = $_POST['user_name'];
-$email = $_POST['user_email'];
+$name = $_GET['user_name'];
 $number = $_POST['user_number'];
 $message = $_POST['user_message'];
-$token = "5222133279:AAFpjNhsMuDOEVEqR3k39PskNhL66oU6plw";
-$chat_id = "-709660835";
-$arr = array(
-  'Имя пользователя: ' => $name,
-  'Email: ' => $email,
-  'Номер: ' => $number,
-  'Сообщение: ' => $message,
-);
+$token = "5472090290:AAG4acoeC6vjL9Jsi3B5Y9j4OLiEYVPk7ks";
+$chat_id = "-1001709791044";
+$arr = array([
+    'Имя пользователя: ' => $name,
+    'Номер: ' => $number,
+    'Сообщение: ' => $message,
+]);
 
 foreach($arr as $key => $value) {
   $txt .= "<b>".$key."</b> ".$value."%0A";
